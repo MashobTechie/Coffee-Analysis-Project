2b0cf6d28e10fbb6b7176861c790d278edb38982
MashobTechie <sheriffdeenmakinde@gmail.com>
2025-09-05 12:00:00 +0100

    Handled statistical analysis: performed ANOVA tests on Sales by Region and Category

--- R_Scripts/statistical_analysis.R
+++ R_Scripts/statistical_analysis.R
@@ -1,7 +1,7 @@
 # Load the cleaned dataset
 coffee_data <- read.csv("./Data/Coffee_Cleaned.csv")
 
-# Convert categorical variables to factor again, since they might get reverted to character after saving
+# Convert categorical variables to factor again, since they might get reverted to character after saving # nolint: line_length_linter.
 coffee_data$Region <- as.factor(coffee_data$Region)
 coffee_data$Category <- as.factor(coffee_data$Category)
 
@@ -9,9 +9,6 @@ coffee_data$Category <- as.factor(coffee_data$Category)
 # The formula `Sales ¬ Region ` tests if Sales are different across regions
 
 anova_model <- aov(Sales ~ Region, data = coffee_data)
-
-# 4. View the results of the ANOVA test
-summary(anova_model)
 
 # Perform an ANOVA test to see if Sales are different across Categories
 anova_model_category <- aov(Sales ~ Category, data = coffee_data)
